9e4dd4416579612f0a16c1235725777f7c360ad9
Fabian Shallari <fabianshallari@gmail.com>
2018-03-13 13:58:53 +0100

    move all screen configuration to screen/configs.js

--- screens/config.js
+++ screens/config.js
@@ -1,9 +1,14 @@
+import ScannerScreen from "./ScannerScreen.js";
+import ResultScreen from "./ResultScreen.js";
+
 export default ScreensConfig = {
   Scanner: {
+    screen: ScannerScreen,
     title: "Gimme Qr Code",
     path: "Scanner"
   },
   Result: {
+    screen: ResultScreen,
     title: "Result Screen",
     path: "Result"
   },
--- screens/navigation.js
+++ screens/navigation.js
@@ -1,18 +1,9 @@
 import { StackNavigator } from "react-navigation";
-import ScannerScreen from "./ScannerScreen.js";
-import ResultScreen from "./ResultScreen.js";
 import ScreensConfig from "./config.js";
 
 export default RootStack = StackNavigator(
   {
-    Scanner: {
-      screen: ScannerScreen,
-      path: ScreensConfig.Scanner.path
-    },
-    Result: {
-      screen: ResultScreen,
-      path: ScreensConfig.Scanner.path
-    }
+    ...ScreensConfig
   },
   {
     initialRouteName: ScreensConfig.Scanner.path
