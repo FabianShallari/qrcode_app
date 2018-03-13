6b3d22b4e183540610dc434bc27dcebfbe06d4c3
Fabian Shallari <fabianshallari@gmail.com>
2018-03-13 15:13:03 +0100

    resolve navigation params based on qr code key

--- screens/ResultScreen.js
+++ screens/ResultScreen.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native'
 
 export default class ResultScreen extends Component {
+  
   render() {
+    const { params } = this.props.navigation.state; 
+    const { height, width } = Dimensions.get('window');
+
     return (
-      <View>
-        <Text> You see result in here </Text>
+      <View style={styles.container}>
+        <Text style={styles.topText}>
+          {params.topText}
+        </Text>
+        <Image
+          source={{uri: params.imageUri}} 
+          style={{
+            flex: 1,
+            borderColor: 'red',
+            width: width,
+          }}
+          resizeMode={'contain'}
+        />
+        <Text style={styles.bottomText}>
+          {params.bottomText}
+        </Text>
       </View>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  topText: {
+    margin: 20,
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  bottomText: {
+    margin: 20,
+    fontSize: 18,
+    fontStyle: 'italic'
+  }
+});
--- screens/ScannerScreen.js
+++ screens/ScannerScreen.js
@@ -51,12 +51,40 @@ export default class ScannerScreen extends Component {
     );
   }
 
-  handleBarCode = () => {
-    const { navigation } = this.props;
+  handleBarCode = (event) => {
     const { focused } = this.state;
+    console.log("QR EVENT: ", event)
 
     if (focused) {
-      navigation.navigate(ScreensConfig.Result.path);
+      const { navigation } = this.props;
+
+      console.log("Data: ", event.data)
+
+      navigation.navigate(ScreensConfig.Result.path, this.paramsForQrCode(event.data));
     }
   }
+
+  paramsForQrCode = (qrData) => {
+    return Object.keys(recognizedQrCodes).includes(qrData) ? recognizedQrCodes[qrData] : fallbackParams;
+  }
+  
 };
+
+const recognizedQrCodes = {
+  dogs: {
+    topText: 'Hug me, cuddle me',
+    imageUri: 'https://i.imgur.com/YikCID7.jpg',
+    bottomText: "C'mon, I'm your friend, your best friend"
+  },
+  cats: {
+    topText: "Gimme Food HOOMAN!!! NOW",
+    imageUri: 'https://i.imgur.com/wYTCtRu.jpg',
+    bottomText: 'What are you still staring at?????'
+  }
+}
+
+const fallbackParams = {
+  topText: "Well, that didn't go as planned now, did it?",
+  imageUri: 'https://i.imgur.com/vkvrmWO.png',
+  bottomText: "Can't trick the master trickster!!!"
+}
